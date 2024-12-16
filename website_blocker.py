114ab2d883a7eea4fd42be119b51caa3c3b7ea7d
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-12-16 19:40:56 +0000

    Update website_blocker.py with new contents

--- website_blocker.py
+++ website_blocker.py
@@ -1,70 +1,82 @@
-import time
-from datetime import datetime
+from tkinter import *
+from tkinter import ttk
+import platform
+import sv_ttk
+import sys
+import pywinstyles
+import darkdetect
 
-adding_websites = True
 
-path = input("Enter in your OS hosts path: ")
-
-redirect = "127.0.0.1"
-
-websites_array = []
-
-while adding_websites:
-
-    website_name = input("Enter in the URL of the website you want to block: ")
-
-    websites_array.append(website_name)
-
-    continue_add = input("Do you want to add another website (y/n)?")
-
-    if continue_add != "Y" or continue_add != "y":
-
-        break
-
-"""
-Hosts Paths On Windows, Mac & Linux:
-
-Windows: C:\Windows\System32\drivers\etc
-
-Mac & Linux: /etc/hosts
-"""
-
-path = input("Enter in your OS hosts path: ")
+h_path = "C:\Windows\System32\drivers\etc\hosts"
 
 redirect = "127.0.0.1"
 
-while True:
+website_arr = []
 
-    if datetime(datetime.now().year, datetime.now().month, datetime.now().day, 8) < datetime.now() < datetime(datetime.now().year, datetime.now().month, datetime.now().day, 16):
+def web_add():
 
-        with open(path, "r+") as web_file:
+    website_arr.append(website_entry.get())
 
-            contents = web_file.read()
+    print(website_arr)
 
-            for i in websites_array:
+def block():
 
-                if i in contents:
+    path = open(h_path, "r+")
 
-                    pass
+    path_r = path.read()
 
-                else:
+    for website in website_arr:
 
-                    web_file.write(f"{redirect} {i}\n")
+        path.write(f"{redirect} {website}\n")
     
-    else:
+def unblock():
+    
+    with open(h_path, "r+") as path:
 
-        with open(path, "r+") as web_file:
+        path_r = path.readlines()
 
-            contents = web_file.readlines()
+        path.seek(0)
 
-            web_file.seek(0)
+        for line in path_r:
+        
+            if not any(website in line for website in website_arr):
 
-            for i in contents:
+                path.write(line)
+        
+        path.truncate()
 
-                if not any(j in i for j in websites_array):
+root = Tk()
 
-                    web_file.write(i)
+root.title("Website Blocker by @SmashedFrenzy16")
 
-            web_file.truncate()
+title_label = ttk.Label(root, text="Website Blocker", font=("Arial", 48))
+title_label.pack()
 
-    time.sleep(5)
+blank_label = ttk.Label(root, text="").pack()
+
+website_entry = ttk.Entry(root, width=100)
+website_entry.insert(0, "Enter website URL here")
+website_entry.pack()
+
+add_website = ttk.Button(root, text="Add", command=web_add)
+add_website.pack()
+
+blank_label2 = ttk.Label(root, text="").pack()
+
+block_button = ttk.Button(root, text="Block", command=block)
+block_button.pack(side=LEFT)
+
+unblock_button = ttk.Button(root, text="Unblock", command=unblock)
+unblock_button.pack(side=RIGHT)
+
+sv_ttk.set_theme(darkdetect.theme())
+
+if platform.system() == "Windows":
+
+    if sys.getwindowsversion().major == 10 and sys.getwindowsversion().build >= 22000:
+
+        if sv_ttk.get_theme() == "dark":
+
+            pywinstyles.change_header_color(root, "#000000")
+
+root.mainloop()
